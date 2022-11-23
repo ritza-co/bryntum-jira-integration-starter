96c635cb9353e82a79b5e0560da0b59147086923
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-11-23 15:13:24 +0200

    Update styling

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
-import { TaskBoard } from '@bryntum/taskboard/taskboard.module.js';
-import { TaskStore } from '@bryntum/taskboard/taskboard.module.js';
+import { TaskBoard, TaskStore } from '@bryntum/taskboard/taskboard.module.js';
 import { getTasksFromJira, updateJira } from './request.js';
-
 
 getTasksFromJira();
 
 function getIssueData(issues) {
-    var issueList = [];
-    issues = issues.issues;
+  const issueList = [];
+  issues = issues.issues;
 
-    for (let i = 0; i < issues.length; i++) {
-        let newIssue = {
-            jiraId: issues[i].key,
-            name: issues[i].fields.summary,
-            status: issues[i].fields.status.name.toLowerCase(),
-        }
-        issueList.push(newIssue);
-    }
-    return issueList;
+  for (let i = 0; i < issues.length; i++) {
+    const newIssue = {
+      jiraId: issues[i].key,
+      name: issues[i].fields.summary,
+      status: issues[i].fields.status.name.toLowerCase(),
+    };
+    issueList.push(newIssue);
+  }
+  return issueList;
 }
 
-
-
 function createTaskBoard(issues) {
+  const taskStore = new TaskStore({
 
-    const taskStore = new TaskStore({
-        
-        listeners: {
-            change: function (event) {
-                updateJira(event); 
-            }},
-        data: getIssueData(issues),
+    listeners: {
+      change: function (event) {
+        updateJira(event);
+      }
+    },
+    data: getIssueData(issues),
 
-    });
+  });
 
-    const taskBoard = new TaskBoard({
+  const taskBoard = new TaskBoard({
 
-        appendTo : "taskboard",
+    appendTo: 'taskboard',
 
-        columnField : 'status',
+    columnField: 'status',
 
-        columns : [
-            "to do",
-            "in progress",
-            "done"
-        ],
+    columns: [
+      'to do',
+      'in progress',
+      'done'
+    ],
 
-        project : {
-            taskStore
-        }
+    project: {
+      taskStore
+    }
 
-    });
+  });
 }
 
 export { createTaskBoard };
--- server.js
+++ server.js
@@ -2,124 +2,115 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 
-
-const atlassianAddress = "<your-atlassian-address-here>";
-const projectKey = "<your-project-key-here>";
-const email = "<your-email-here>";
-const token = "<your-api-token-here>";
+const atlassianAddress = '<your-atlassian-address-here>';
+const projectKey = '<your-project-key-here>';
+const email = '<your-email-here>';
+const token = '<your-api-token-here>';
 const transitionList = {
-    "to do": "11",
-    "in progress": "21",
-    "done": "31",
-}
+  'to do': '11',
+  'in progress': '21',
+  'done': '31',
+};
 const authHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    'Authorization': `Basic ${Buffer.from(
-        `${email}:${token}`
-    ).toString('base64')}`,
-}
+  'Access-Control-Allow-Origin': '*',
+  // eslint-disable-next-line quote-props
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': `Basic ${Buffer.from(
+    `${email}:${token}`,
+  ).toString('base64')}`,
+};
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:8080'
+  origin: 'http://localhost:8080',
 }));
-app.use(express.json())  
+app.use(express.json());
 
-
+// eslint-disable-next-line prefer-arrow-callback
 app.get('/getIssues', function (req, res) {
-
-    const response = fetch(`${atlassianAddress}/2/search?jql=project=${projectKey}`, {  
+  fetch(`${atlassianAddress}/2/search?jql=project=${projectKey}`, {
     method: 'GET',
     headers: authHeaders,
-    })
-    .then(res => res.json())
-    .then(json => res.send(json))
-    
-})
-
+  })
+    .then((res) => res.json())
+    .then((json) => res.send(json));
+});
 
 app.put('/updateName', function (req, res) {
-
-    const response = fetch(`${atlassianAddress}/2/issue/${req.body.key}`, {  
+  fetch(`${atlassianAddress}/2/issue/${req.body.key}`, {
     method: 'PUT',
     headers: authHeaders,
     body: JSON.stringify({
-        "update": {
-            "summary": [
-            {
-                "set": req.body.summary
-            }
-            ],
-        },
-    })
+      'update': {
+        'summary': [
+          {
+            'set': req.body.summary,
+          }
+        ],
+      },
+    }),
 
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-})
-
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+});
 
 app.post('/addIssue', function (req, res) {
-
-    let transitionType = transitionList[req.body.status];
-    const bodyData =   {
-        "fields": {
-            "summary": req.body.summary,
-            "project": {
-                "key": "JDW"
-            },
-            "issuetype": {
-                "id": "10001"
-              },
-        },
-        "transition": {
-            "id": transitionType
-          }
+  const transitionType = transitionList[req.body.status];
+  const bodyData = {
+    'fields': {
+      'summary': req.body.summary,
+      'project': {
+        'key': 'JDW',
+      },
+      'issuetype': {
+        'id': '10001',
+      },
+    },
+    'transition': {
+      'id': transitionType,
     }
+  };
 
-    const response = fetch(`${atlassianAddress}/3/issue`, {
-        method: 'POST',
-        headers: authHeaders,
-        body: JSON.stringify(bodyData)
-    })
-    .then(res => res.json())
-    .then(json => res.send(json))
-    .catch(err => console.log(err))
-})
+  fetch(`${atlassianAddress}/3/issue`, {
+    method: 'POST',
+    headers: authHeaders,
+    body: JSON.stringify(bodyData),
+  })
+    .then((res) => res.json())
+    .then((json) => res.send(json))
+    .catch((err) => console.log(err));
+});
 
 app.delete('/deleteIssue', function (req, res) {
-    const response = fetch(`${atlassianAddress}/3/issue/${req.body.key}`, {
-        method: 'DELETE',
-        headers: authHeaders,
-      })
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
-})
-
+  fetch(`${atlassianAddress}/3/issue/${req.body.key}`, {
+    method: 'DELETE',
+    headers: authHeaders,
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+});
 
 app.post('/updateTransition', function (req, res) {
+  const transitionType = transitionList[req.body.status];
 
-    let transitionType = transitionList[req.body.status];
+  fetch(`${atlassianAddress}/2/issue/${req.body.key}/transitions`, {
+    method: 'POST',
+    headers: authHeaders,
+    body: JSON.stringify({
+      'transition': {
+        'id': transitionType,
+      }
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+});
 
-    const response = fetch(`${atlassianAddress}/2/issue/${req.body.key}/transitions`, {
-        method: 'POST',
-        headers: authHeaders,
-        body: JSON.stringify({
-            "transition": {
-                "id": transitionType
-            }
-        })
-    })
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
-})
-
-
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
-    });
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
